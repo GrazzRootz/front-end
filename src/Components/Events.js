2b119dbdf48960252e65c97492cdeb7cbe180009
Sam Marshall <sjmarshy@gmail.com>
2019-10-26 22:48:39 +0100

    refactor with useState

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,30 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import axios from 'axios';
 
-export class Events extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            events: []
-        }
-    }
+const Event = (event) => {
+    return (
+        <div className='event' key={event.uuid}>
+            <h2 className='event-title'>{event.title}</h2>
+            <div>
+                <p>{event.date}</p>
+                <p>{event.desc}</p>
+                <p>garden link TODO: {event.garden}</p>
+            </div>
+        </div>
+    );
+};
 
-    componentDidMount() {
-        axios.get('http://localhost:4000/event').then(data => {
-            this.setState(old => ({ events: data.data.events }));
-        });
-    }
+export const Events = () => {
+    const [events, setEvents] = useState([]);
+    const [error, setError] = useState(undefined);
+    const { loading, user } = useAuth0();
 
-    render() {
-        // const { loading }  = useAuth0();
-        return this.state.events.length === 0 ?
-            <Fragment>loading</Fragment> :
-            <Fragment><li>{
-                this.state.events.map(event => 
-                    <ul key={event.uuid}>
-                        {event.title}
-                    </ul>)
-            }</li></Fragment>;
-    }
+    axios.get('http://localhost:4000/event')
+        .then(data => data.data.events)
+        .then(setEvents)
+        .catch(setError);
+
+
+    return loading 
+        ? <>Loading...</> 
+        : error 
+        ? <div><p>Error! {error.message}</p></div> 
+        : events.length > 0
+        ? events.map(e => <Event>{e}</Event>)
+        : <div><p>no events!</p></div>;
 }
