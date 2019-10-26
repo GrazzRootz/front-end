e8dd607e93492696c2084454f3b1844aef7ba1b3
jrawling <rawling.jacob@gmail.com>
2019-10-26 20:29:53 +0100

    Added bootstrap

---
+++ src/Components/Polls/PollForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PollForm extends React.Component {
+    render() {
+      return (
+        <h1>{this.props.title}</h1>;
+      )
+    }
+  }
+
+export default PollForm;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Auth0Provider } from './react-auth0-spa';
 import config from './auth_config.json';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // A function that routes the user to the right place
 // after login
