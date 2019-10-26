58cd40d013338ebd75fb3cd13d72a0e0802ab1e0
njblore <njblore@outlook.com>
2019-10-26 22:22:07 +0100

    add buttons for showing gardens and the plants in them

--- src/Components/GardenAPI.js
+++ src/Components/GardenAPI.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { useAuth0 } from "../react-auth0-spa";
 
 const GardenAPI = () => {
-  const [showResult, setShowResult] = useState(false);
-  const [apiMessage, setApiMessage] = useState("");
+  const [showGardens, setShowGardens] = useState(false);
+  const [showPlants, setShowPlants] = useState(false);
+  const [gardens, setGardens] = useState("");
+  const [plants, setPlants] = useState("");
   const { getTokenSilently } = useAuth0();
   const baseURL = 'http://localhost:3001/api';
 
   const getGardens = async () => {
-    try {
+    if (!showGardens) {
+       try {
       const token = await getTokenSilently();
       const response = await fetch(`${baseURL}/gardens`, {
         headers: {
@@ -16,20 +19,50 @@ const GardenAPI = () => {
           "Access-Control-Allow-Origin": '*',
         }
       });
-
       const responseData = await response.json();
-      setShowResult(true);
-      setApiMessage(responseData);
+      setShowGardens(true);
+      setGardens(responseData);
     } catch (error) {
       console.error(error);
     }
+    } else {
+      setShowGardens(false);
+      setShowPlants(false);
+    }
+   
   };
+
+  const getPlantsInGarden = async (id) => {
+
+          try {
+      const token = await getTokenSilently();
+      const response = await fetch(`${baseURL}/plants/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Access-Control-Allow-Origin": '*',
+        }
+      });
+      const responseData = await response.json();
+      setShowPlants(true);
+      setPlants(responseData);
+    } catch (error) {
+      console.error(error);
+    }
+   
+
+  }
 
   return (
     <>
       <h1>Garden API</h1>
       <button onClick={getGardens}>Get gardens</button>
-      {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+      {showGardens&& gardens && gardens.gardens.map(garden => 
+      <>
+        <p>{garden.garden_name}</p>
+        <button onClick={() => getPlantsInGarden(garden.garden_id)}>Show plants in this garden</button>
+        </>
+      )}
+      {showPlants && plants && plants.plants.map(plant => <p>{plant.plant_name}: {plant.quantity}</p>)}
     </>
   );
 };
