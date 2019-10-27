c9374ffa51ad8a175fc5ec059a4e7a55b4527f8b
Sam Marshall <sjmarshy@gmail.com>
2019-10-27 08:50:31 +0000

    add landing page styles

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -40,7 +40,6 @@ const NavBar = () => {
       </Nav>
       }
     </Navbar>
->>>>>>> master
   );
 };
 
--- src/Components/NewCommunity.js
+++ src/Components/NewCommunity.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
 import { Redirect } from 'react-router-dom';
 import { useAuth0 } from '../react-auth0-spa';
+
+const CommunityCard = ({ img = 'angel-meadow.png', title = 'Angel Meadow', text = 'Our Meadow is a pretty nice place. We like seed bombs and other sweet stuff', ...rest }) => {
+    return (
+        <Col><Card {...rest}>
+            <Card.Img variant='top' src={img}/>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {text}
+                </Card.Text>
+            </Card.Body>
+        </Card></Col>
+    );
+}
 
 const MakeNewCommunity = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect({})}>Log In</button>;
+    return (
+        <Container>
+            <Row style={{ paddingTop: '16px' }}>
+                <Col>
+                    <h1 style={{ textAlign: 'center' }}>Welcome to Grazzrootz</h1>
+                    <h2 style={{ textAlign: 'center' }}>We Reap What We Sow</h2>
+                </Col>
+            </Row>
+            <Row style={{ paddingTop: '32px' }}>
+                <Col>
+                    <p style={{ textAlign: 'center' }}>Join other communities in taking control of your green space</p>
+                </Col>
+            </Row>
+            <Row style={{ paddingTop: '8px' }}>
+                <Col style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button  onClick={() => loginWithRedirect({})}>New Community</Button>
+                </Col>
+            </Row>
+            <Row style={{ paddingTop: '32px' }}>
+                    <CommunityCard style={{ width: 220 }} />
+                    <CommunityCard style={{ width: 220 }}/>
+            </Row>
+            <Row style={{ paddingTop: '8px' }}>
+                    <CommunityCard style={{ width: 220 }} />
+                    <CommunityCard style={{ width: 220 }}/>
+            </Row>
+            <Row style={{ paddingTop: '16px' }}>
+                <Col style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button  onClick={() => loginWithRedirect({})}>New Community</Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export const NewCommunity = () => {
